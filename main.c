6f22e5f14fec7f0d358c3c151806c188e4ef09f9
1220505061 <127785239+1220505061@users.noreply.github.com>
2023-05-15 21:53:46 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+//Yýðýn (Stack) ve Kuyruk (Queue), veri yapýlarýndan ikisidir ve temelde verilerin saklanmasýný ve eriþimini yönetmek için kullanýlýrlar. Ýþlevleri ve çalýþma prensipleri birbirinden farklýdýr. 
+
+/*Yýðýn (Stack):
+
+Yýðýn, verilerin LIFO (Son giren, ilk çýkar) prensibiyle saklandýðý bir veri yapýsýdýr.
+Elemanlar yalnýzca bir uçtan (top) eklenir ve çýkarýlýr.
+En son eklenen eleman, ilk çýkarýlýr (son giren, ilk çýkar mantýðý).
+Eleman eklemek için "push" iþlemi kullanýlýrken, eleman çýkarmak için "pop" iþlemi kullanýlýr.
+Yýðýnda gezinme sadece en üstteki elemana (top) eriþim saðlar.
+Bir örnekle ifade etmek gerekirse, yýðýna kitap eklersek en son eklenen kitap en üstte durur ve ilk olarak çýkarýlýr.*/
+
+/*Kuyruk (Queue):
+
+Kuyruk, verilerin FIFO (Ýlk giren, ilk çýkar) prensibiyle saklandýðý bir veri yapýsýdýr.
+Elemanlar bir uçtan (arka) eklenir ve diðer uçtan (ön) çýkarýlýr.
+En önce eklenen eleman, ilk çýkarýlýr (ilk giren, ilk çýkar mantýðý).
+Eleman eklemek için "enqueue" iþlemi kullanýlýrken, eleman çýkarmak için "dequeue" iþlemi kullanýlýr.
+Kuyrukta gezinme, kuyruðun önünden arkasýna doðru yapýlýr.
+Bir örnekle ifade etmek gerekirse, bir kuyruða insanlar eklersek, ilk giren insan ilk olarak kuyruktan çýkar.*/
+
+
